ae1c92863ed5d76f0c82f964bf5c413581ac8e4a
Gabriel Carneiro <gmiqueiascarneiro52@gmail.com>
2025-06-13 20:50:11 -0300

    fix: fixed the username and password validation

--- src/user/dtos/create-user.dto.ts
+++ src/user/dtos/create-user.dto.ts
@@ -3,8 +3,8 @@ import { IsString, IsEmail, MinLength, MaxLength, IsNotEmpty, Matches } from "cl
 export class CreateUserDto {
     @IsNotEmpty({ message: "The username must not be empty!" })
     @IsString({ message: "The username must be a string!" })
-    @MinLength(3, { message: "The username must have a 3 characters min size!" })
-    @MaxLength(12, { message: "The username must have a 12 characters max size!" })
+    @MinLength(3, { message: "The username must have at least 3 characters!" })
+    @MaxLength(12, { message: "The username's length is greater than 12 characters!" })
     username: string;
 
     @IsNotEmpty({ message: "The email must not be empty!" })
@@ -12,7 +12,7 @@ export class CreateUserDto {
     email: string;
 
     @IsNotEmpty({ message: "The password must not be empty!" })
-    @MinLength(6, { message: "The password must have a 6 characters min size!" })
+    @MinLength(6, { message: "The password must have at least 6 character!" })
     @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/, { message: "The password must contain lowercase characters, uppercase characters, numbers and symbols" })
     password: string;
 }
